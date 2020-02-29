df4fa47f10623afd143bd7597904c0a5aa4bf073
Susheel <susheelk@wickedride.com>
2020-02-29 09:36:04 +0530

    Using a new script to tunnel to staging

--- db_setup.sh
+++ db_setup.sh
@@ -1,5 +1,3 @@
-#staging postgres config. Local port 4567 configured to postgres
-ssh -f -N -L 4567:bounce-staging-rds.cavqxjfmp25f.ap-south-1.rds.amazonaws.com:5432 $1@10.7.0.1
+#!/usr/bin/env bash
 
-#staging redis config. Local port 1234 configured to redis.
-ssh -f -N -L 1234:redis-staging-bcore.n6pp0w.0001.aps1.cache.amazonaws.com:6379 $1@10.7.0.1
+echo "./db_setup.sh is deprecated. Please use ./tunnel_staging.sh instead."
