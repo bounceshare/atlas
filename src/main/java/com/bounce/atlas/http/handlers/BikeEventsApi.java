3ea9f118006ae66901f63be87427f608058c0c1d
Susheel <susheelk@wickedride.com>
2020-03-02 13:46:48 +0530

    Using redis for bsl

--- src/main/java/com/bounce/atlas/http/handlers/BikeEventsApi.java
+++ src/main/java/com/bounce/atlas/http/handlers/BikeEventsApi.java
@@ -1,28 +1,30 @@
 package com.bounce.atlas.http.handlers;
 
 import com.bounce.atlas.pojo.BikeDetailsCard;
-import com.bounce.atlas.utils.QueryUtils;
+import com.bounce.utils.BounceUtils;
 import com.bounce.utils.DatabaseConnector;
+import com.bounce.utils.GlobalConfigUtils;
+import com.bounce.utils.RealtimeData;
 import com.bounce.utils.apis.BaseApiHandler;
 import com.bounce.utils.dbmodels.public_.tables.BikeStatusLog;
 import com.bounce.utils.dbmodels.public_.tables.Booking;
 import com.bounce.utils.dbmodels.public_.tables.EndTripFeedback;
-import com.bounce.utils.dbmodels.public_.tables.records.BikeRecord;
 import com.bounce.utils.dbmodels.public_.tables.records.BikeStatusLogRecord;
 import com.bounce.utils.dbmodels.public_.tables.records.BookingRecord;
 import com.bounce.utils.dbmodels.public_.tables.records.EndTripFeedbackRecord;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.joda.time.DateTime;
+import redis.clients.jedis.Jedis;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.container.AsyncResponse;
+import java.lang.reflect.Type;
 import java.sql.Timestamp;
-import java.util.Collections;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class BikeEventsApi extends BaseApiHandler {
 
@@ -77,11 +79,15 @@ public class BikeEventsApi extends BaseApiHandler {
                 bslLastTime = new DateTime(from).minusDays(2).getMillis();
             }
 
-            List<BikeStatusLogRecord> bikeStatusLogRecords =
-                    DatabaseConnector.getDb().getReadDbConnector().selectFrom(BikeStatusLog.BIKE_STATUS_LOG)
-                            .where(BikeStatusLog.BIKE_STATUS_LOG.BIKE_ID.eq(bikeId))
-                            .and(BikeStatusLog.BIKE_STATUS_LOG.CREATED_ON.greaterThan(new Timestamp(bslLastTime)))
-                            .and(BikeStatusLog.BIKE_STATUS_LOG.CREATED_ON.lessThan(new Timestamp(bslFirstTime))).fetch();
+            List<BikeStatusLogRecord> bikeStatusLogRecords = null;
+            if(GlobalConfigUtils.getGlobalConfigBoolean("ATLAS_USE_BSL_FROM_REDIS", true)) {
+                bikeStatusLogRecords = getBsl(bikeId, bslFirstTime, bslLastTime);
+            } else {
+                bikeStatusLogRecords = DatabaseConnector.getDb().getReadDbConnector().selectFrom(BikeStatusLog.BIKE_STATUS_LOG)
+                                .where(BikeStatusLog.BIKE_STATUS_LOG.BIKE_ID.eq(bikeId))
+                                .and(BikeStatusLog.BIKE_STATUS_LOG.CREATED_ON.greaterThan(new Timestamp(bslLastTime)))
+                                .and(BikeStatusLog.BIKE_STATUS_LOG.CREATED_ON.lessThan(new Timestamp(bslFirstTime))).fetch();
+            }
 
             for(BikeStatusLogRecord bsl : bikeStatusLogRecords) {
                 bikeDetailsCards.add(BikeDetailsCard.getCard(bsl));
@@ -95,6 +101,35 @@ public class BikeEventsApi extends BaseApiHandler {
         response.put("events", bikeDetailsCards);
 
         sendSuccessResponse(asyncResponse, response);
+    }
+
+    private static List<BikeStatusLogRecord> getBsl(int bikeId, long startTime, long endTime) {
+        Set<String> bikeStatusLogSet = new HashSet<>();
+        List<BikeStatusLogRecord> bikeStatusLogRecords = Lists.newArrayList();
+        try {
+            try (Jedis jedis = RealtimeData.get().getJedisFactory().getTrackingJedisConnection(1)) {
+                bikeStatusLogSet = jedis.zrevrangeByScore("bike_status_created_on_bike_id_" + bikeId,
+                        endTime, startTime);
+            } catch (Exception e) {
+                logger.info("Exception while fetching BSL for bike " + bikeId);
+                BounceUtils.logError(e);
+                e.printStackTrace();
+            }
+
+            for(String bslString : bikeStatusLogSet) {
+                Type type = new TypeToken<Map<String, Object>>(){}.getType();
+                Map<String, Object> map = new Gson().fromJson(bslString, type);
+                BikeStatusLogRecord record = new BikeStatusLogRecord();
+                record.fromMap(map);
+
+                bikeStatusLogRecords.add(record);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            BounceUtils.logError(e);
+        }
+
+        return bikeStatusLogRecords;
     }
 
 }
