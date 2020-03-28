9e497136b03dc5ddaba0baa6627b55e9fd90fd9a
Susheel <susheelk@wickedride.com>
2020-03-28 17:14:48 +0530

    Fixed some html

--- src/main/resources/scripts/render.js
+++ src/main/resources/scripts/render.js
@@ -73,7 +73,7 @@
                     legend = markerData.title;
                 }
                 var markerIcon = L.divIcon({
-                   className: 'bg-transparent',
+                   className: 'bg-transparent text-center',
                    html: '<span class="text-center" style="order: 1; position: relative; background-color: #fff; border-radius: 5px; border-width: 2px; border-style: solid; border-color: #444; padding: 3px; white-space: nowrap;">' + legend +'</span><img src="' + markerData.iconUrl + '" style="width: 50; height: 50;">',
                    iconUrl: markerData.iconUrl,
                    iconSize:     [50, 50], // size of the icon
